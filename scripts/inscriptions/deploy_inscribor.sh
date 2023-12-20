5488d9ff19046f3430b9f1bf89ae59ddded5ac5f
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2023-12-20 09:49:19 +0100

    🚧

---
+++ scripts/inscriptions/deploy_inscribor.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+###############################################
+# DECLARE THE CONTRACT CLASS                  #
+###############################################
+
+# Prepare declare args
+COMPILER_VERSION="2.1.0"
+CONTRACT_CLASS_FILE="./target/dev/unruggable_UnruggableMemecoin.contract_class.json"
+DECLARE_ARGS="--compiler-version=$COMPILER_VERSION"
+
+# Declare the contract and capture the command output
+command_output=$(starkli declare $CONTRACT_CLASS_FILE $DECLARE_ARGS --watch)
+
+from_string="Class hash declared:"
+class_hash="${command_output#*$from_string}"
+
+echo "Class hash: $class_hash"
+
+###############################################
+# DEPLOY THE CONTRACT                         #
+###############################################
+
+# Deploy the contract using the extracted class hash
+starkli deploy $class_hash
--- src/inscriptions/inscribor.cairo
+++ src/inscriptions/inscribor.cairo
@@ -5,11 +5,25 @@ trait IInscribor<TContractState> {
 
 #[starknet::contract]
 mod Inscribor {
+    use core::starknet::event::EventEmitter;
     #[storage]
     struct Storage {}
 
+    #[event]
+    #[derive(Drop, starknet::Event)]
+    enum Event {
+        Inscribed: Inscribed,
+    }
+
+    #[derive(Drop, starknet::Event)]
+    struct Inscribed {
+        user_data: Array<felt252>,
+    }
+
     #[external(v0)]
     impl InscriborImpl of super::IInscribor<ContractState> {
-        fn inscribe(ref self: ContractState, user_data: Array<felt252>) {}
+        fn inscribe(ref self: ContractState, user_data: Array<felt252>) {
+            self.emit(Inscribed { user_data });
+        }
     }
 }
